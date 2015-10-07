41ea49a21c121eab96e55fc4e12c41109fc0088b
Nicholas Stobie <npstobie@gmail.com>
2015-10-07 14:13:52 -0700

    Implemented dequeue to delete the first item in the obj and track what is occurring

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -6,13 +6,31 @@ var Queue = function() {
 
   // Implement the methods below
 
+  var length = 0;
+  var dequeue = 0;
+
   someInstance.enqueue = function(value) {
+    storage[length+dequeue] = value;
+    length++;
   };
 
   someInstance.dequeue = function() {
+    var val;
+    if (length > 0) {
+          val = storage[dequeue];
+    delete storage[dequeue];
+    console.log(dequeue)
+    length--;      
+    dequeue++;
+    }
+
+    console.log(dequeue);
+    console.log(storage);
+    return val;
   };
 
   someInstance.size = function() {
+    return length;
   };
 
   return someInstance;
