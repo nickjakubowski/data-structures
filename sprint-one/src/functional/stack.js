6d8a6d5a6d926b05cac31fccdddd8ffa2ef9fd46
Nicholas Stobie <npstobie@gmail.com>
2015-10-07 12:05:34 -0700

    initialized the length property at 0. .push adds the value at key length then increments the value of length by one. .pop decrements the value of length by one that deletes the value at storage[length]. should've committed earlier, stil working on it :)

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,14 +4,26 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var length = 0;
+
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[length] = value;
+    length++;
   };
 
   someInstance.pop = function() {
+    var poppedItem;
+    if (length > 0) {
+      length--;    
+    }
+    poppedItem = storage[length];
+    delete storage[length]; 
+    return poppedItem;
   };
 
   someInstance.size = function() {
+    return length;
   };
 
   return someInstance;
