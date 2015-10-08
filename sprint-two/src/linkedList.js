56b003020b6fd610b02a8628e92b6a459d185553
nickjakubowski <ncjakubowski@gmail.com>
2015-10-08 15:05:56 -0700

    Added comments to help understanding

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -5,34 +5,51 @@ var LinkedList = function() {
 
   list.addToTail = function(value) {
     var newNode = Node(value);
+    // if no items have been added to the list
     if (this.head === null){
+      // then both head and tail point to the newly added node 
       this.head = newNode;
       this.tail = newNode;
+      // else if there is already at least one item in the list
     } else {
+      // the current tails next property now points to the newly added node
       this.tail.next = newNode;
+      //the newly added node becomes the new tail property of the list
       this.tail = newNode;
     }    
   };
 
   list.removeHead = function() {
+    //removed head equals the current head
     var removedHead = this.head;
+    //this.head now becomes the next node in the list
     this.head = this.head.next;
+    // return the value of the removed head
     return removedHead.value;
   };
 
   list.contains = function(target) {
+    // set the search default to false
     var included = false;
+    // recursive function
     var listChecker = function(val) {
+      // if the current nodes value property is equal to the target set
+      // the included variable to true and return out of the function
       if (val.value === target) {
         included = true;
         return
+        // else if val.next === null (AKA if were on the .last item) return 
+        // out of the function
       } else if (val.next === null) {
         return
+        // else call the listChecker function of the next obj in the list
       } else {
         listChecker(val.next)
       }
     };
+    // call the listChecker function on the head of the list
     listChecker(this.head);
+    // return true or false
     return included;
   };
 
