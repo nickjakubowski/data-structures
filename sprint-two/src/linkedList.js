788a31032839c93779665498658b0c68b1e3f25e
nickjakubowski <ncjakubowski@gmail.com>
2015-10-08 14:20:22 -0700

    Started fresh on linkedList.js and smashed it. Each new item added to the list becomes the new tail and also the .next property of the previous tail

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,19 +4,36 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
-    if (this.head === null) {
-      this.head = Node(value);
-    }
-    this.tail = Node(value);
-    this.tail.next = Node(value);
-    console.log(list);
+    var newNode = Node(value);
+    if (this.head === null){
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }    
   };
 
   list.removeHead = function() {
-    this.head = this.next;
+    var removedHead = this.head;
+    this.head = this.head.next;
+    return removedHead.value;
   };
 
   list.contains = function(target) {
+    var included = false;
+    var listChecker = function(val) {
+      if (val.value === target) {
+        included = true;
+        return
+      } else if (val.next === null) {
+        return
+      } else {
+        listChecker(val.next)
+      }
+    };
+    listChecker(this.head);
+    return included;
   };
 
   return list;
@@ -30,6 +47,51 @@ var Node = function(value) {
 
   return node;
 };
+
+
+
+
+
+
+
+
+
+// var LinkedList = function() {
+//   var list = {};
+//   list.head = null;
+//   list.tail = null;
+
+//   list.addToTail = function(value) {
+//     var newNode = Node(value);
+//     if (this.head === null) {
+//       this.head = newNode;
+//       this.tail = newNode;
+//     } else {
+//     this.tail.next = newNode;
+//     this.tail = newNode;
+//     console.log(list);  
+//     }
+//   };
+
+//   list.removeHead = function() {
+//     this.head = this.next;
+//     delete this.head
+//   };
+
+//   list.contains = function(target) {
+//   };
+
+//   return list;
+// };
+
+// var Node = function(value) {
+//   var node = {};
+
+//   node.value = value;
+//   node.next = null;
+
+//   return node;
+// };
 
 /*
  * Complexity: What is the time complexity of the above functions?
