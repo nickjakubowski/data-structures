2ae125f368f90d25712b3df513e7c3cbce846cb7
nickjakubowski <ncjakubowski@gmail.com>
2015-10-08 16:13:49 -0700

    Implemented tree maker function. has the methods addChildren which adds a child object to the specified parent and also has the .contains method which recursively iterates through the tree and checks for a specified value

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,22 +1,40 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  _.extend(newTree, treeMethods);
+  // sets each objects children property to an empty array
+  newTree.children = [];
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  // your code here
-  newTree.children = null;  // fix me
+  // turns each value into an instance of the Tree class
+  // and pushes that object to the children array of the 
+  //current node
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+    // sets boolean to represents presence of object, defaults to false
+    var truthy = false;
+    // if this current object's value property is equal to the
+    // target or if truty = true, return true
+    if (this.value === target || truthy) {
+      return true;
+    // else loop through the current objects children property
+    // and call that childs contain method recursively.
+    } else {
+      for (var i = 0; i < this.children.length; i++) {
+        if (this.children[i].contains(target)) {
+          truthy = true;
+        }
+      }
+    }
+  return truthy;
 };
+
 
 
 
