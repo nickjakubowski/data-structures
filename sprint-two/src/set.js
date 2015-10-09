873afab7929ef571e9337da5b1e6e2bdc8f8fa84
nickjakubowski <ncjakubowski@gmail.com>
2015-10-09 12:33:40 -0700

    Implemented all methods for set. Adds, removes, and checks if the set contains an item

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,19 +1,32 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  set._storage = undefined;
+  if (this._storage.indexOf(item) === -1) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  for (var i=0; i<this._storage.length; i++) {
+    if (this._storage[i] = item) {
+      return true
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  for (var i=0; i<this._storage.length; i++) {
+    if (this._storage[i] === item) {
+      this._storage.splice(i, 1)
+    }
+  }
 };
 
 /*
