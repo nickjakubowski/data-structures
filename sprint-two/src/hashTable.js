dc922ff3ddf77417a20307e9eec9055f981163b5
nickjakubowski <ncjakubowski@gmail.com>
2015-10-09 21:29:32 -0700

    Implemented a HashTable using only arrays. Didn't use the LimitedArray file HR created for us as we weren't aware but will work on another implementation tomorrow

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,18 +3,43 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this.table = [];
 };
 
-HashTable.prototype.insert = function(k, v) {
+HashTable.prototype.insert = function(k, v) { 
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (!Array.isArray(this.table[index])) {
+    this.table[index] = [];
+    this.table[index].push([k, v]);
+  } else {
+    for (var i=0; i<this.table[index].length; i++) {
+      if (this.table[index][i][0] === k) {
+        this.table[index][i][1] = v
+      } else if (this.table[index].length - 1 === i) {
+        this.table[index].push([k,v])
+      }
+    }
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  for (var i=0; i<this.table[index].length; i++) {
+    if (this.table[index][i][0] === k) {
+      return this.table[index][i][1]
+    } 
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  for (var i=0; i<this.table[index].length; i++) {
+    if (this.table[index][i][0] === k) {
+      this.table[index][i][1] = null;
+      console.log(this.table[index][i])
+    } 
+  }
+
 };
 
 
