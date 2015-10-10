2794deea0fc1e44de73a3400e818c18c97e89fc4
nickjakubowski <ncjakubowski@gmail.com>
2015-10-10 11:43:16 -0700

    Created a recursive method to add values to a binary tree object. Working on a contains mehthod.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,78 @@
 var BinarySearchTree = function(value) {
+  var newNode = Object.create(bsTreeMethods)
+  newNode.value = value;
+  newNode.left = null;
+  newNode.right = null;
+  return newNode;
 };
+
+var bsTreeMethods = {};
+
+bsTreeMethods.insert = function(val) {
+  if (val > this.value) {
+    if (this.right !== null) {
+      this.right.insert(val)
+    } else {
+      this.right = BinarySearchTree(val);
+    }
+  } else if (val <= this.value) {
+    if (this.left !== null) {
+      this.left.insert(val)
+    } else {
+      this.left = BinarySearchTree(val);
+    }
+  }
+}
+
+bsTreeMethods.contains = function(value){
+  var truthy = false;
+  var traverser = function(val) {
+    if (this.left === null && this.right === null) {
+      return
+    } else if (this.value > val) {
+      if (this.left === null) {
+        return
+      } else {
+        this.left.contains.traverser(val)
+      }
+    } else if (this.val < val) {
+      if (this.right === null) {
+        return
+      } else {
+        this.right.contains.traverser(val);
+      }
+    } else if (this.value === val) {
+      truthy = true;
+      return;
+    }
+  }
+  traverser(value);
+  return truthy;
+}
+
+// bsTreeMethods.contains = function(val) {
+// //   var found = present || false
+// //   if (this.left === null && this.right === null) {
+// //     found = false;
+// //   } else if (present) {
+// //     found = true;
+// //   } else if (this.value === val) {
+// //     found = true;
+// //   } else if (this.value > val) {
+// //     this.left.contains(val, found);
+// //   } else if (this.value < val) {
+// //     this.right.contains(val, found)
+// //   } 
+// //   return found;
+// }
+
+bsTreeMethods.depthFirstLog = function(val) {
+
+}
+
+
+
+
 
 
 /*
